c186061e6b10d7bdec70d3ac9c90e82f127a75d4
ElvisFMM <elvismercado967@gmail.com>
2023-11-07 21:01:54 -0800

    game_data.sqlite filled with values

---
+++ bulkload.sql
@@ -0,0 +1,11 @@
+.mode csv
+.separator "|"
+
+.import table_data/coaches.tbl coaches
+.import table_data/games.tbl games
+.import table_data/mascot.tbl mascots
+.import table_data/played_for.tbl played_for
+.import table_data/players.tbl players
+.import table_data/seasons.tbl seasons
+.import table_data/stadiums.tbl stadiums
+.import table_data/teams.tbl teams

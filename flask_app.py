07eb9d0ecdb38c3f8a556bed2236fedbd45a5d4b
ElvisFMM <elvismercado967@gmail.com>
2023-12-04 20:44:52 -0800

    updated routes

--- flask_app.py
+++ flask_app.py
@@ -57,7 +57,7 @@ def index_Q1():
 
     return render_template('Q1.html')
 
-#route for Q1
+#route for Q2
 @app.route('/Q2.html')
 def index_Q2():
 
@@ -65,5 +65,117 @@ def index_Q2():
     return render_template('Q2.html')
 
 
+#route for Q3
+@app.route('/Q3.html')
+def index_Q3():
+
+
+    return render_template('Q3.html')
+
+#route for Q4
+@app.route('/Q4.html')
+def index_Q4():
+
+
+    return render_template('Q4.html')
+
+#route for Q5
+@app.route('/Q5.html')
+def index_Q5():
+
+
+    return render_template('Q5.html')
+
+#route for Q6
+@app.route('/Q6html')
+def index_Q6():
+
+
+    return render_template('Q6.html')
+
+
+#route for Q7
+@app.route('/Q7.html')
+def index_Q7():
+
+
+    return render_template('Q7.html')
+
+#route for Q8
+@app.route('/Q8.html')
+def index_Q8():
+
+
+    return render_template('Q8.html')
+
+#route for Q9
+@app.route('/Q9.html')
+def index_Q9():
+
+
+    return render_template('Q9.html')
+
+#route for Q10
+@app.route('/Q10.html')
+def index_Q10():
+
+
+    return render_template('Q10.html')
+
+#route for Q11
+@app.route('/Q11.html')
+def index_Q11():
+
+
+    return render_template('Q11.html')
+
+#route for Q12
+@app.route('/Q12.html')
+def index_Q12():
+
+
+    return render_template('Q12.html')
+
+#route for Q13
+@app.route('/Q13.html')
+def index_Q13():
+
+
+    return render_template('Q13.html')
+
+#route for Q14
+@app.route('/Q14.html')
+def index_Q14():
+
+
+    return render_template('Q14.html')
+
+#route for Q15
+@app.route('/Q15.html')
+def index_Q15():
+
+
+    return render_template('Q15.html')
+
+#route for Q16
+@app.route('/Q16.html')
+def index_Q16():
+
+
+    return render_template('Q16.html')
+
+#route for Q3
+@app.route('/Q17.html')
+def index_Q17():
+
+
+    return render_template('Q17.html')
+
+#route for Q18
+@app.route('/Q18.html')
+def index_Q18():
+
+
+    return render_template('Q18.html')
 
 
